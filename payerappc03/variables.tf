054f993e63f93cdd93757fc8349d860b0941bbe8
Bob Bauer <rotbau@gmail.com>
2024-07-11 17:17:15 -0500

    updated

--- payerappc03/variables.tf
+++ payerappc03/variables.tf
@@ -146,3 +146,25 @@ variable "nodepool1_vm_class" {
   description = "VM size for nodepool 1 nodes"
   type        = string
 }
+
+// TMC Backup Schedule Variables
+
+variable "backup_job_name" {
+  description = "Name for Job"
+  type        = string
+}
+
+variable "backup_scope" {
+  description = "Cluster level"
+  type        = string
+}
+
+variable "storage_location" {
+  description = "Storage Target in TMC for Backups"
+  type        = string
+}
+
+variable "excluded_namespaces" {
+  description = "Namespace to skip on backup"
+  type        = list(string)
+}
