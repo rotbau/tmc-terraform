9d4cb7c4d264a1627d7c2e0e9583fd01a51e4056
Bob Bauer <rotbau@gmail.com>
2024-07-11 17:31:26 -0500

    updated

---
+++ payerappc03/terraform.tf
@@ -0,0 +1,18 @@
+// TMC terraform provider initialization
+
+terraform {
+  required_providers {
+    tanzu-mission-control = {
+      source = "vmware/tanzu-mission-control"
+      version = "1.4.4"
+      configuration_aliases = [ tanzu-mission-control ]
+    }
+  }
+}
+
+// TMC Org configuration
+
+provider "tanzu-mission-control" {
+  endpoint            = var.vmw_tmc_endpoint // Required, TMC Org URL
+  vmw_cloud_api_token = var.vmw_cloud_api_token   // Required, API Token
+}
