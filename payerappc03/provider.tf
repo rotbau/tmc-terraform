119355a06c2b88a67017b435726fe13bc86f6ccb
Bob Bauer <rotbau@gmail.com>
2024-07-11 17:38:43 -0500

    updated

--- payerappc03/provider.tf
+++ payerappc03/provider.tf
@@ -5,7 +5,6 @@ terraform {
     tanzu-mission-control = {
       source = "vmware/tanzu-mission-control"
       version = "1.4.4"
-      configuration_aliases = [ tanzu-mission-control ]
     }
   }
 }
@@ -16,3 +15,5 @@ provider "tanzu-mission-control" {
   endpoint            = var.vmw_tmc_endpoint // Required, TMC Org URL
   vmw_cloud_api_token = var.vmw_cloud_api_token   // Required, API Token
 }
+
+//variable "SESSION_NAMESPACE" {}
