05bf93d4fe79cb1ed44a697aa644346df8299065
Bob Bauer <rotbau@gmail.com>
2024-07-11 15:37:35 -0500

    updated provider

--- payerappc03/main.tf
+++ payerappc03/main.tf
@@ -6,6 +6,7 @@ terraform {
       source = "vmware/tanzu-mission-control"
       version = "1.4.4"
     }
+    alias = tmc
   }
 }
 
@@ -19,19 +20,13 @@ provider "tanzu-mission-control" {
 module "tkgs_cluster" {
   source = "../modules/tkgs-cluster" 
   providers = {
-    tanzu-mission-control = {
-      source = "vmware/tanzu-mission-control"
-      version = "1.4.4"
-    }
+    tanzu-mission-control = tanzu-mission-control.tmc
   }
 }
 
 module "tmc_backup_schedule" {
   source = "../modules/tmc-backup"
   providers = {
-    tanzu-mission-control = {
-      source = "vmware/tanzu-mission-control"
-      version = "1.4.4"
-    }
+    tanzu-mission-control = tanzu-mission-control.tmc
   }
 }
