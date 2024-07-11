4ef2668301823f5a2a443f48f7e7919ea8f22556
Bob Bauer <rotbau@gmail.com>
2024-07-11 17:23:58 -0500

    updated

--- modules/tkgs-cluster/main.tf
+++ modules/tkgs-cluster/main.tf
@@ -1,3 +1,20 @@
+// TMC terraform provider initialization
+
+terraform {
+  required_providers {
+    tanzu-mission-control = {
+      source = "vmware/tanzu-mission-control"
+      version = "1.4.4"
+    }
+  }
+}
+
+provider "tanzu-mission-control" {
+  endpoint            = var.vmw_tmc_endpoint // Required, TMC Org URL
+  vmw_cloud_api_token = var.vmw_cloud_api_token   // Required, API Token
+}
+
+
 data "tanzu-mission-control_cluster_class" "tkc-cc" {
   name                    = var.cluster_name
   management_cluster_name = var.management_cluster_name
