737349122dbde411f8f74da5bf8989bfcfbbd3e1
Bob Bauer <rotbau@gmail.com>
2024-07-12 15:18:46 -0500

    multi-cluster workflow

--- payerappc01/main.tf
+++ payerappc01/main.tf
@@ -1,32 +1,65 @@
+variable "clusterlist" {
+  description = "List of clusters to create"
+  type = map(any)
+
+  default = {
+    dev-cluster = {
+      name_prefix = teama-dev
+      tkr_version = "v1.28.8+vmware.1-fips.1-tkg.2"
+      cp_replicas = 1
+      cp_vm_class = "best-effort-small"      
+      np1_replicas = 2
+      np1_vm_class = "best-effort-medium"
+    }
+    stage-cluster = {
+      name_prefix = teama-stage
+      tkr_version = "v1.28.8+vmware.1-fips.1-tkg.2"
+      cp_replicas = 1
+      cp_vm_class = "best-effort-small"      
+      np1_replicas = 2
+      np1_vm_class = "best-effort-medium"
+    }
+    prod-cluster = {
+      name_prefix = teama-prod
+      tkr_version = "v1.28.8+vmware.1-fips.1-tkg.2"
+      cp_replicas = 1
+      cp_vm_class = "best-effort-small"      
+      np1_replicas = 2
+      np1_vm_class = "best-effort-medium"
+    }
+  }
+}
+
 resource "random_integer" "ri" {
   min = 1000
   max = 9999
 }
 
 module "tkgs_cluster" {
+  for_each = var.clusterlist
   source = "../modules/tkgs-cluster"
 
   vmw_cloud_api_token = var.vmw_cloud_api_token
   vmw_tmc_endpoint = var.vmw_tmc_endpoint
   management_cluster_name = var.management_cluster_name
   provisioner_name = var.provisioner_name
-  cluster_name = "${var.cluster_name}-${random_integer.ri.result}"
+  cluster_name = "${each.value.name_prefix}-${random_integer.ri.result}"
   cluster_group = var.cluster_group
   pod_cidr_blocks = var.pod_cidr_blocks
   service_cidr_blocks = var.service_cidr_blocks
-  tkr_version = var.tkr_version
+  tkr_version = each.value.tkr_version
   cluster_ntp = var.cluster_ntp
   cluster_storageclass = var.cluster_storageclass
   cluster_storageclasses = var.cluster_storageclasses
-  cluster_vm_class = var.cluster_vm_class
+  cluster_vm_class = each.value.cp_vm_class
   cluster_trusted_ca1 = var.cluster_trusted_ca1
-  control_plane_replicas = var.control_plane_replicas
+  control_plane_replicas = each.value.cp_replicas
   control_plane_os_name = var.control_plane_os_name
   control_plane_os_version = var.control_plane_os_version
   control_plane_os_arch = var.control_plane_os_arch
   nodepool1_name = var.nodepool1_name
   nodepool1_description = var.nodepool1_description
-  nodepool1_worker_replicas = var.nodepool1_worker_replicas
+  nodepool1_worker_replicas = each.value.np1_replicas
   nodepool1_worker_class = var.nodepool1_worker_class
   nodepool1_os_name = var.nodepool1_os_name
   nodepool1_os_version = var.nodepool1_os_version
@@ -34,7 +67,7 @@ module "tkgs_cluster" {
   nodepool1_label1_key = var.nodepool1_label1_key
   nodepool1_label1_value = var.nodepool1_label1_value
   nodepool1_storageclass = var.nodepool1_storageclass
-  nodepool1_vm_class = var.nodepool1_vm_class
+  nodepool1_vm_class = each.value.np1_vm_class
 }
 
 module "tmc_backup_schedule" {
