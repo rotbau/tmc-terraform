accb708c504550e53581da0ef5261d17ae229c48
Bob Bauer <rotbau@gmail.com>
2024-07-11 19:28:14 -0500

    updated

--- payerappc03/main.tf
+++ payerappc03/main.tf
@@ -42,7 +42,7 @@ module "tmc_backup_schedule" {
 
   management_cluster_name = var.management_cluster_name
   provisioner_name = var.provisioner_name
-  cluster_name = "${module.tkgs_cluster.name}"
+  cluster_name = "${module.tkgs_cluster.tanzu-mission-control_tanzu_kubernetes_cluster.tkgs_cluster.name}"
   vmw_cloud_api_token = var.vmw_cloud_api_token
   vmw_tmc_endpoint = var.vmw_tmc_endpoint
   backup_job_name = var.backup_job_name
