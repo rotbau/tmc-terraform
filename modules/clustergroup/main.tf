63ad55d8dfa6122bf280687bcb8bc08df42cffe7
Bob Bauer <rotbau@gmail.com>
2024-07-12 13:51:35 -0500

    moved to module

--- modules/clustergroup/main.tf
+++ modules/clustergroup/main.tf
@@ -101,10 +101,10 @@ resource "tanzu-mission-control_kustomization" "create_cluster_group_kustomizati
   }
 }
 # Create Tanzu Mission Control cluster group scope helm feature with attached set as default value.
-resource "tanzu-mission-control_helm_feature" "create_cg_helm_feature" {
-  scope {
-    cluster_group {
-      name = var.cluster_group # Required
-    }
-  }
-}
+#resource "tanzu-mission-control_helm_feature" "create_cg_helm_feature" {
+#  scope {
+#    cluster_group {
+#      name = var.cluster_group # Required
+#    }
+#  }
+#}
