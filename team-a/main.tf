ee124dd64777727974bf2b509cd94f11ff68b28d
Bob Bauer <rotbau@gmail.com>
2024-07-15 11:11:51 -0500

    multi-cluster build

--- team-a/main.tf
+++ team-a/main.tf
@@ -1,11 +1,45 @@
+locals {
+  clusterlist = {
+  description = "List of clusters to create"
+  type = map(any)
+
+  default = {
+    dev-cluster = {
+      name_prefix = "teama-dev"
+      tkr_version = "v1.28.8+vmware.1-fips.1-tkg.2"
+      cp_replicas = 1
+      cp_vm_class = "best-effort-small"      
+      np1_replicas = 2
+      np1_vm_class = "best-effort-medium"
+    }
+    stage-cluster = {
+      name_prefix = "teama-stage"
+      tkr_version = "v1.28.8+vmware.1-fips.1-tkg.2"
+      cp_replicas = 1
+      cp_vm_class = "best-effort-small"      
+      np1_replicas = 2
+      np1_vm_class = "best-effort-medium"
+    }
+    prod-cluster = {
+      name_prefix = "teama-prod"
+      tkr_version = "v1.28.8+vmware.1-fips.1-tkg.2"
+      cp_replicas = 1
+      cp_vm_class = "best-effort-small"      
+      np1_replicas = 2
+      np1_vm_class = "best-effort-medium"
+    }
+  }
+  }
+
+}
 resource "random_integer" "ri" {
-  for_each = var.clusterlist
+  for_each = local.clusterlist
   min = 1000
   max = 9999
 }
 
 module "tkgs_cluster" {
-  for_each = var.clusterlist
+  for_each = local.clusterlist
   source = "../modules/tkgs-cluster"
   
 
