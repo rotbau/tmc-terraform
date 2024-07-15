d178e3588e942a2132bdfafd2952af145de8bbb4
Bob Bauer <rotbau@gmail.com>
2024-07-15 12:12:56 -0500

    fixed cluster var

--- modules/tkgs-cluster/main.tf
+++ modules/tkgs-cluster/main.tf
@@ -123,6 +123,6 @@ resource "tanzu-mission-control_tanzu_kubernetes_cluster" "tkgs_cluster" {
 }
 
 output "tkg-cluster-name" {
-  value = tanzu-mission-control_tanzu_kubernetes_cluster.tkgs_cluster.name[each.key]
+  value = tanzu-mission-control_tanzu_kubernetes_cluster.tkgs_cluster.name[0]
   
 }
