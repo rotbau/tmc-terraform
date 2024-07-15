20e19061e088e95d3a25ddf2cb79a4f22c8c9d24
Bob Bauer <rotbau@gmail.com>
2024-07-15 09:26:55 -0500

    fixed variable

--- team-a/variables.tf
+++ team-a/variables.tf
@@ -6,7 +6,7 @@ variable "clusterlist" {
 
   default = {
     dev-cluster = {
-      name_prefix = teama-dev
+      name_prefix = "teama-dev"
       tkr_version = "v1.28.8+vmware.1-fips.1-tkg.2"
       cp_replicas = 1
       cp_vm_class = "best-effort-small"      
@@ -14,7 +14,7 @@ variable "clusterlist" {
       np1_vm_class = "best-effort-medium"
     }
     stage-cluster = {
-      name_prefix = teama-stage
+      name_prefix = "teama-stage"
       tkr_version = "v1.28.8+vmware.1-fips.1-tkg.2"
       cp_replicas = 1
       cp_vm_class = "best-effort-small"      
@@ -22,7 +22,7 @@ variable "clusterlist" {
       np1_vm_class = "best-effort-medium"
     }
     prod-cluster = {
-      name_prefix = teama-prod
+      name_prefix = "teama-prod"
       tkr_version = "v1.28.8+vmware.1-fips.1-tkg.2"
       cp_replicas = 1
       cp_vm_class = "best-effort-small"      
