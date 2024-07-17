6c0166bb8041ca59f924e6c9d8e9b3f9fad46fcf
Bob Bauer <rotbau@gmail.com>
2024-07-17 13:41:47 -0500

    fixed main.tf provider statement

--- modules/aks-cluster/main.tf
+++ modules/aks-cluster/main.tf
@@ -9,13 +9,6 @@ terraform {
   }
 }
 
-// TMC Org configuration
-
-provider "tanzu-mission-control" {
-  endpoint            = var.vmw_tmc_endpoint // Required, TMC Org URL
-  vmw_cloud_api_token = var.vmw_cloud_api_token   // Required, API Token
-}
-
 //variable "SESSION_NAMESPACE" {}
 
 resource "tanzu-mission-control_akscluster" "demo_AKS_cluster" {
