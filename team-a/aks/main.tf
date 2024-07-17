a8a8e6c2a8d3a045301e36a4d5acd75904b02533
Bob Bauer <rotbau@gmail.com>
2024-07-17 13:52:25 -0500

    fixed cluster name variable

--- team-a/aks/main.tf
+++ team-a/aks/main.tf
@@ -1,3 +1,9 @@
+resource "random_integer" "ri" {
+#  for_each = var.clusterlist
+  min = 1000
+  max = 9999
+}
+
 module "aks_cluster" {
   for_each = var.clusterlist
   source = "../../modules/aks-cluster"
@@ -7,7 +13,7 @@ vmw_tmc_endpoint = var.vmw_tmc_endpoint
 credential_name = var.credential_name
 azure_subscription_id = var.azure_subscription_id
 resource_group = each.value.resource_group
-aks_clustername = each.value.aks_clustername
+aks_clustername = "${each.value.name_prefix}-${random_integer.ri.result}"
 location = each.value.location
 k8s_version = each.value.k8s_version
 node_count = each.value.node_count
