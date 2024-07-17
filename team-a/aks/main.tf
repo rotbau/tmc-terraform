7b3d0f52bb275680a8c328f72489c1f24e1fb232
Bob Bauer <rotbau@gmail.com>
2024-07-17 18:19:39 -0500

    fixed backup module

--- team-a/aks/main.tf
+++ team-a/aks/main.tf
@@ -13,8 +13,6 @@ vmw_tmc_endpoint = var.vmw_tmc_endpoint
 credential_name = var.credential_name
 azure_subscription_id = var.azure_subscription_id
 resource_group = each.value.resource_group
-management_cluster_name = var.management_cluster_name
-provisioner_name = var.provisioner_name
 aks_clustername = "${each.value.name_prefix}-${random_integer.ri.result}"
 location = each.value.location
 k8s_version = each.value.k8s_version
@@ -35,6 +33,8 @@ module "tmc_backup_schedule" {
 
   vmw_cloud_api_token = var.vmw_cloud_api_token
   vmw_tmc_endpoint = var.vmw_tmc_endpoint
+  management_cluster_name = var.management_cluster_name
+  provisioner_name = var.provisioner_name
   cluster_name = module.aks_cluster[each.key].aks_clustername
   backup_job_name = var.backup_job_name
   backup_scope = var.backup_scope
