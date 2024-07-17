00ae48905cfd6d671b5e7c81814197a200128eb4
Bob Bauer <rotbau@gmail.com>
2024-07-17 10:47:54 -0500

    fixed main.tf vars

--- aks-team-a/main.tf
+++ aks-team-a/main.tf
@@ -3,12 +3,12 @@ module "aks_cluster" {
 
 vmw_cloud_api_token = var.vmw_cloud_api_token
 vmw_tmc_endpoint = var.vmw_tmc_endpoint
-credential_name = ""
-azure_subscription_id = ""
-resource_group =   ""
-aks_clustername = ""
-location = ""
-k8s_version = "" 
-node_count = ""
-vm_size = ""
+credential_name = var.credential_name
+azure_subscription_id = var.azure_subscription_id
+resource_group =   var.resource_group
+aks_clustername = var.aks_clustername
+location = var.location
+k8s_version = var.k8s_version
+node_count = var.node_count
+vm_size = var.vm_size
 }
