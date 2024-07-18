b30440963658215df616a3ed83f6b4ecbe4325ad
Bob Bauer <rotbau@gmail.com>
2024-07-17 19:07:58 -0500

    updated custername for backup

--- team-a/aks/main.tf
+++ team-a/aks/main.tf
@@ -35,7 +35,7 @@ module "tmc_backup_schedule" {
   vmw_tmc_endpoint = var.vmw_tmc_endpoint
   management_cluster_name = var.management_cluster_name
   provisioner_name = var.provisioner_name
-  cluster_name = module.aks_cluster[each.key].aks_cluster_name
+  cluster_name = module.aks_cluster[each.key]
   backup_job_name = var.backup_job_name
   backup_scope = var.backup_scope
   storage_location = var.storage_location
