11420abebe54d4985b330a5a1da1da90cda63868
Bob Bauer <rotbau@gmail.com>
2024-07-18 07:57:20 -0500

    removed backup config

--- modules/aks-cluster/outputs.tf
+++ modules/aks-cluster/outputs.tf
@@ -1,6 +1,6 @@
-output "aks_cluster_name" {
-  value = instances[0].attributes.spec[0].agent_name
-}
+#output "aks_cluster_name" {
+#  value = instances[0].attributes.spec[0].agent_name
+#}
 
 output "kubeConfig" {
   value = tanzu-mission-control_akscluster.AKS_cluster.kubeconfig
