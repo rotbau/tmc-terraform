3e7474777f050da8ef1244e6c55ebf735959a62b
Bob Bauer <rotbau@gmail.com>
2024-07-17 20:20:31 -0500

    still

--- modules/aks-cluster/outputs.tf
+++ modules/aks-cluster/outputs.tf
@@ -1,5 +1,5 @@
 output "aks_cluster_name" {
-  value = tanzu-mission-control_akscluster.AKS_cluster.instances[0].attributes.spec[0].agent_name
+  value = tanzu-mission-control_akscluster.AKS_cluster.instances.attributes.spec[0].agent_name
 }
 
 output "kubeConfig" {
